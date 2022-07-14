5161e510e04cab46db2a3a1cee7f5d9f0b52f12c
Graham Rogers <grahamr@adobe.com>
2022-07-14 14:58:53 -0600

    fix some uh-ohs

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -14,7 +14,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
-    plugins: ['simple-import-sort'],
+    plugins: ["simple-import-sort"],
     rules: {
         "@next/next/no-html-link-for-pages": "off",
         "react/jsx-key": "off",
