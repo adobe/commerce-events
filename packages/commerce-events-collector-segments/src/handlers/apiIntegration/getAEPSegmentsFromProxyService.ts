d2c0d328efef1ded6d48bd6945a0695d75196f00
Jason Poteet <jasonrpoteet@gmail.com>
2022-08-01 14:50:57 -0500

    SEG-8: [segment-collector] Proxy Service call for AEP segmentation
    - fixing build errors dealing with proper typing

--- packages/commerce-events-collector-segments/src/handlers/apiIntegration/getAEPSegmentsFromProxyService.ts
+++ packages/commerce-events-collector-segments/src/handlers/apiIntegration/getAEPSegmentsFromProxyService.ts
@@ -3,35 +3,35 @@
  * as the default `options` should be fine.
  */
 const apiFetchOptions: RequestInit = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
     headers: {
-        'content-type': 'application/json;charset=UTF-8',
-    }
+        "content-type": "application/json;charset=UTF-8",
+    },
 };
 
 /**
- * 
+ * Call segments proxy service and get a list of segment ids for current user
+ *
  * @param profileId - ECID of current user, should be retrieved from Ally
- * 
+ *
  * @returns Promise with returend data from adobe segment proxy service
  */
-const getAEPSegmentsFromProxyService = (profileId:string): Promise<string | void> => {
-
+const getAEPSegmentsFromProxyService = (profileId: string): Promise<string | void> => {
     // static api_key that is used for the proxy service
-    const API_KEY = "commerce-segments-service"; 
+    const API_KEY = "commerce-segments-service";
 
     // static imsOrgId used for the proxy service, this should eventually come from Alloy, but this script needs to be integrated into the extension to test access to the `imsOrgId`
     const imsOrgId = "53A16ACB5CC1D3760A495C99@AdobeOrg";
-    
+
     // URL to access the segments proxy service, this will most likely come from a config file or service after POC
     const GET_AEP_SEGMENTS_ENDPOINT_URL = `https://commerce-int.adobe.io/segments/segments-service/profiles/${profileId}}/segmentmemberships?imsOrgId=${imsOrgId}}&api_key=${API_KEY}`;
 
     return new Promise((resolve, reject): string | void => {
         fetch(GET_AEP_SEGMENTS_ENDPOINT_URL, apiFetchOptions)
-            .then(response => response.json())
-            .then(responseData => {
+            .then((response) => response.json())
+            .then((responseData) => {
                 /*
                     JSON object that comes back is in a structure like: 
                     {
@@ -47,12 +47,12 @@ const getAEPSegmentsFromProxyService = (profileId:string): Promise<string | void
                     So we need to just get `segmentMembershipIds` and combine that into a comma delimited string.  
                     @Note: Once this is finalized could just destructure it out of the `responseData`, i.e. ({data:{segmentMembershipIds = []} = {}})
                 */
-                resolve(responseData?.data?.segmentMembershipIds?.join(",") || "")
+                resolve(responseData?.data?.segmentMembershipIds?.join(",") || "");
             })
-            .catch(error => {
+            .catch((error) => {
                 reject(error);
-            })
-    }
+            });
+    });
 };
 
 export default getAEPSegmentsFromProxyService;
--- packages/commerce-events-collector-segments/src/index.ts
+++ packages/commerce-events-collector-segments/src/index.ts
@@ -1,24 +1,19 @@
-import { getAEPSegmentsFromProxyService } from "src/handlers/apiIntegration";
-import { setAdobeCommerceSegmentCookies } from "src/handlers/browserCookieIntegration";
-import { getECID } from "src/handlers/alloyIntegration";
+import { getAEPSegmentsFromProxyService } from "./handlers/apiIntegration";
+import { setAdobeCommerceSegmentCookies } from "./handlers/browserCookieIntegration";
+import { getECID } from "./handlers/alloyIntegration";
 
 /**
  * Initialize the commerce-events-collector workflow
  */
 const initialize = async () => {
-    getECID()
-        .then((ecid: string) => {
-            getAEPSegmentsFromProxyService(ecid)
-                .then((segmentMembershipIds: string) => {
-                    setAdobeCommerceSegmentCookies(segmentMembershipIds)
-                })
-                .catch(error => {
-                    console.error("Error on getting proxy service data; ", error);
-                })
-        })
-        .catch(error => {
-            console.error("Error on getting `ECID`: ", error);
-        })
+    try {
+        const ecid: string = (await getECID()) || "";
+        const segmentMembershipIds: string = (await getAEPSegmentsFromProxyService(ecid)) || "";
+
+        setAdobeCommerceSegmentCookies(segmentMembershipIds);
+    } catch (error) {
+        console.warn("Error on getting segments: ", error);
+    }
 };
 
 initialize();
