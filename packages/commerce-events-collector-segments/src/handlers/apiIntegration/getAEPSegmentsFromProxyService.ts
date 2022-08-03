735b4f789b74db78538d9965ff7af2da2c0a5e92
Jason Poteet <jasonrpoteet@gmail.com>
2022-08-03 11:22:43 -0500

    Feature/commerce segments seg 8 (#24)
    * SEG-8: [segment-collector] Proxy Service call for AEP segmentation
    
    - removing fetch headers
    - cleaning up console logs

--- packages/commerce-events-collector-segments/src/handlers/apiIntegration/getAEPSegmentsFromProxyService.ts
+++ packages/commerce-events-collector-segments/src/handlers/apiIntegration/getAEPSegmentsFromProxyService.ts
@@ -6,49 +6,24 @@
  * @returns Promise with returend data from adobe segment proxy service
  */
 const getAEPSegmentsFromProxyService = (profileId: string): Promise<string | void> => {
-    console.log("commerce-event-segmnets - getAEPSegmentsFromProxyService start: ", profileId);
     // static api_key that is used for the proxy service
     const API_KEY = "commerce-segments-service";
 
     // get a reference to the magento storefront events context. This will give us access to the AEP data
     const { context } = window.magentoStorefrontEvents;
     const { imsOrgId = "" } = context.getAEP();
-    console.log("commerce-event-segmnets - getAEPSegmentsFromProxyService imsOrgId: ", imsOrgId);
 
     // URL to access the segments proxy service, this will most likely come from a config file or service after POC
     const GET_AEP_SEGMENTS_ENDPOINT_URL = `https://commerce-int.adobe.io/segments/segments-service/profiles/${profileId}/segmentmemberships?imsOrgId=${imsOrgId}&api_key=${API_KEY}`;
 
     return new Promise((resolve, reject): string | void => {
         fetch(GET_AEP_SEGMENTS_ENDPOINT_URL)
-            .then((response) => {
-                console.log("commerce-event-segmnets - getAEPSegmentsFromProxyService fetch response: ", response);
-                const responseJson = response.json();
-                console.log("commerce-event-segmnets - getAEPSegmentsFromProxyService fetch json: ", responseJson);
-                console.log("commerce-event-segmnets - getAEPSegmentsFromProxyService fetch text: ", response.text());
-
-                return responseJson;
-            })
+            .then((response) => response.json())
             .then((responseData) => {
-                /*
-                    JSON object that comes back is in a structure like: 
-                    {
-                        "data": {
-                            "segmentMembershipIds": [
-                                "abc-123",
-                                "def-456",
-                                "ghi-789"
-                            ]
-                        },
-                        "signature": "<hashed signature string>"
-                    }
-                    So we need to just get `segmentMembershipIds` and combine that into a comma delimited string.  
-                    @Note: Once this is finalized could just destructure it out of the `responseData`, i.e. ({data:{segmentMembershipIds = []} = {}})
-                */
-                console.log("commerce-event-segmnets - AEP Segments Fetch returned: ", responseData);
+                console.log("commerce-event-segmnets - getAEPSegmentsFromProxyService fetch response: ", responseData);
                 resolve(responseData?.data?.segmentMembershipIds?.join(",") || "");
             })
             .catch((error) => {
-                console.log("commerce-event-segmnets - AEP Segments Fetch error: ", error);
                 reject(error);
             });
     });
