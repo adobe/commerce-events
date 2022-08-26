76db5146e10bc8f2702dd3257d2bde7f0b177bff
Ben Batschelet <batschel@adobe.com>
2022-08-26 13:06:29 -0500

    Modify commerce collector & sdk default ports

--- packages/commerce-events-collectors/rollup.config.js
+++ packages/commerce-events-collectors/rollup.config.js
@@ -99,7 +99,7 @@ export default [
             !isProduction &&
                 serve({
                     contentBase: "dist",
-                    port: 8081,
+                    port: 8083,
                     headers: {
                         "Access-Control-Allow-Origin": "*",
                     },
