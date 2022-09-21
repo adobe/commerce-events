176e54822719fd75dc366653b9b9974be8ccae4a
Benjamin Kalk <bkalk@adobe.com>
2022-09-21 17:57:42 -0500

    update snowplow addToCart

--- packages/storefront-events-collector/src/handlers/product/addToCart.ts
+++ packages/storefront-events-collector/src/handlers/product/addToCart.ts
@@ -4,22 +4,33 @@ import { SelfDescribingJson, trackStructEvent } from "@snowplow/browser-tracker"
 import { createProductCtx, createShoppingCartCtx } from "../../contexts";
 
 const handler = (event: Event): void => {
-    const { pageContext, productContext, shoppingCartContext } = event.eventInfo;
-
-    const productCtx = createProductCtx(productContext);
-    const shoppingCartCtx = createShoppingCartCtx(shoppingCartContext);
-
-    const context: Array<SelfDescribingJson> = [productCtx];
-
-    if (shoppingCartCtx) {
-        context.push(shoppingCartCtx);
-    }
-
-    trackStructEvent({
-        category: "product",
-        action: "add-to-cart",
-        property: pageContext?.pageType,
-        context,
+    const { changedProductsContext, pageContext, productContext, shoppingCartContext } = event.eventInfo;
+    // console.log("CHANGED PRODUCTS:");
+    // console.log(changedProductsContext.items);
+    // console.log("PRODUCT CONTEXT:");
+    // console.log(productContext);
+    changedProductsContext.items?.forEach((item) => {
+        let productCtx;
+        if(item.product.sku === productContext.sku){
+            productCtx = createProductCtx(productContext);
+        } else {
+            productCtx = createProductCtx(item.product)
+        }
+        console.log(productCtx);
+        const shoppingCartCtx = createShoppingCartCtx(shoppingCartContext);
+    
+        const context: Array<SelfDescribingJson> = [productCtx];
+    
+        if (shoppingCartCtx) {
+            context.push(shoppingCartCtx);
+        }
+    
+        trackStructEvent({
+            category: "product",
+            action: "add-to-cart",
+            property: pageContext?.pageType,
+            context,
+        });
     });
 };
 
