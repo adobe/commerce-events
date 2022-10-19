781812498a67fe12e4651a5bcb84eb4f5919b541
Jason Poteet <JasonRPoteet@gmail.com>
2022-10-19 09:50:16 -0500

    SEG-339: [segment-collector-segments] Multiple events showing up on AEP because of retry
    - removed old segment names as its no longer needed and any remains of it should be removed by now
    - updated segment id call from alloy to only be called once
    - added in a "retry" for checking if alloy is available and then making alloy calls

--- packages/commerce-events-collector-segments/src/handlers/browserCookieIntegration/setAdobeCommerceSegmentCookies.ts
+++ packages/commerce-events-collector-segments/src/handlers/browserCookieIntegration/setAdobeCommerceSegmentCookies.ts
@@ -2,16 +2,14 @@
  * The name of the cookie to save so that adobe commerce can read in the segment information.
  *
  * Should match up with Github located at:
- * @link https://github.com/magento-commerce/segments-service/blob/poc/Segment/Model/SegmentResolver.php#L12
+ * @link https://github.com/magento-commerce/segments-service/blob/main/Segment/Model/SegmentResolver.php#L13
  */
-const ADOBE_COMMERCE_SHOPPER_SEGMENT = "adobe_segment";
 const ADOBE_COMMERCE_AEP_SEGMENT_MEMBERSHIP_COOKIE_NAME = "aep-segments-membership";
 
 /**
  * Clear the browsers cookies set for Adobe Commerce AEP Segments
  */
 export const clearAdobeCommerceAEPSegmentCookies = () => {
-    document.cookie = `${ADOBE_COMMERCE_SHOPPER_SEGMENT}=; expires=Fri, 31 Dec 1999 23:59:59 GMT;`;
     document.cookie = `${ADOBE_COMMERCE_AEP_SEGMENT_MEMBERSHIP_COOKIE_NAME}=; expires=Fri, 31 Dec 1999 23:59:59 GMT;`;
 };
 
