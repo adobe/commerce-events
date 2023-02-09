69be0404ec11df055ea2400289542789c88000e9
Graham Rogers <grahamr@adobe.com>
2023-02-09 09:49:33 -0800

    release v1.2.1 (#68)
    * bump version number for release
    
    * fixing tests

--- packages/storefront-events-collector/tests/contexts/product.test.ts
+++ packages/storefront-events-collector/tests/contexts/product.test.ts
@@ -3,10 +3,13 @@ import schemas from "../../src/schemas";
 import { mockProductCtx } from "../utils/mocks";
 
 test("creates context", () => {
+    // we don't send product id to snowplow anymore, but don't want to break mocks for any aep tests
+    const { productId, ...mockProductCtxNoId } = mockProductCtx;
+
     const ctx = createProductCtx();
 
     expect(ctx).toEqual({
-        data: mockProductCtx,
+        data: mockProductCtxNoId,
         schema: schemas.PRODUCT_SCHEMA_URL,
     });
 });
