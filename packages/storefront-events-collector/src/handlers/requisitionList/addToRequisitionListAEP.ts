c4fa893ece7531efa8bac945e3f8e5ab1443b6fd
Nadiya Syvokonenko <syvokone@adobe.com>
2023-03-31 13:42:34 -0500

    DINT-889: convert Requisition List Id to string

--- packages/storefront-events-collector/src/handlers/requisitionList/addToRequisitionListAEP.ts
+++ packages/storefront-events-collector/src/handlers/requisitionList/addToRequisitionListAEP.ts
@@ -3,6 +3,7 @@ import { sendEvent } from "../../alloy";
 import { BeaconSchema } from "../../types/aep";
 import { createProductListItems } from "../../utils/aep/productListItems";
 import { createProductListItemsFromRequisitionListItems } from "../../utils/aep/requisitionList";
+import { createRequisitionList } from "../../utils/aep/requisitionList";
 
 const XDM_EVENT_TYPE = "commerce.requisitionListAdds";
 
