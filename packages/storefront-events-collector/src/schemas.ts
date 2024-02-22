c8340edb2e9ffa6586b80b9014a769a5abfc66d5
Kayden Althen <althen@adobe.com>
2024-02-22 14:24:35 -0600

    update schema.ts

--- packages/storefront-events-collector/src/schemas.ts
+++ packages/storefront-events-collector/src/schemas.ts
@@ -6,17 +6,17 @@ const schemas = {
     EXPERIENCE_PLATFORM_CONNECTOR_EXTENSION_SCHEMA_URL:
         "iglu:com.adobe.magento.entity/experience-platform-connector-extension/jsonschema/1-0-1",
     MAGENTO_JS_TRACKER_SCHEMA_URL: "iglu:com.adobe.magento.entity/magento-js-tracker/jsonschema/2-0-0",
-    PRODUCT_SCHEMA_URL: "iglu:com.adobe.magento.entity/product/jsonschema/3-0-0",
-    RECOMMENDATION_UNIT_SCHEMA_URL: "iglu:com.adobe.magento.entity/recommendation-unit/jsonschema/1-0-4",
-    RECOMMENDED_ITEM_SCHEMA_URL: "iglu:com.adobe.magento.entity/recommended-item/jsonschema/1-0-4",
-    SEARCH_INPUT_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-input/jsonschema/2-0-0",
+    PRODUCT_SCHEMA_URL: "iglu:com.adobe.magento.entity/product/jsonschema/2-0-7",
+    RECOMMENDATION_UNIT_SCHEMA_URL: "iglu:com.adobe.magento.entity/recommendation-unit/jsonschema/1-0-5",
+    RECOMMENDED_ITEM_SCHEMA_URL: "iglu:com.adobe.magento.entity/recommended-item/jsonschema/1-0-5",
+    SEARCH_INPUT_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-input/jsonschema/2-0-1",
     SEARCH_RESULT_CATEGORY_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-result-category/jsonschema/1-0-1",
-    SEARCH_RESULT_PRODUCT_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-result-product/jsonschema/1-0-2",
-    SEARCH_RESULTS_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-results/jsonschema/2-0-0",
+    SEARCH_RESULT_PRODUCT_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-result-product/jsonschema/1-0-3",
+    SEARCH_RESULTS_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-results/jsonschema/2-0-1",
     SEARCH_RESULT_SUGGESTION_SCHEMA_URL: "iglu:com.adobe.magento.entity/search-result-suggestion/jsonschema/1-0-1",
-    SHOPPING_CART_SCHEMA_URL: "iglu:com.adobe.magento.entity/shopping-cart/jsonschema/2-0-0",
+    SHOPPING_CART_SCHEMA_URL: "iglu:com.adobe.magento.entity/shopping-cart/jsonschema/2-0-1",
     SHOPPER_SCHEMA_URL: "iglu:com.adobe.magento.entity/shopper/jsonschema/1-0-0",
-    STOREFRONT_INSTANCE_SCHEMA_URL: "iglu:com.adobe.magento.entity/storefront-instance/jsonschema/3-0-1",
+    STOREFRONT_INSTANCE_SCHEMA_URL: "iglu:com.adobe.magento.entity/storefront-instance/jsonschema/3-0-2",
 };
 
 export default schemas;
