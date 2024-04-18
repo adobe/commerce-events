d3549db2ea1c649976dcf2d429dd6e91a75db78f
Benjamin Kalk <bkalk@adobe.com>
2024-04-18 14:03:58 -0500

    enforce numeric discount amount

--- packages/storefront-events-collector/src/handlers/checkout/placeOrderAEP.ts
+++ packages/storefront-events-collector/src/handlers/checkout/placeOrderAEP.ts
@@ -10,53 +10,57 @@ const XDM_EVENT_TYPE = "commerce.purchases";
 
 /** Sends an event to aep with a checkout complete payload */
 const aepHandler = async (event: Event): Promise<void> => {
-    const {
-        accountContext,
-        storefrontInstanceContext,
-        orderContext,
-        shoppingCartContext,
-        debugContext,
-        customContext,
-    } = event.eventInfo;
+    try{
+        const {
+            accountContext,
+            storefrontInstanceContext,
+            orderContext,
+            shoppingCartContext,
+            debugContext,
+            customContext,
+        } = event.eventInfo;
 
-    let payload: BeaconSchema = {};
-    if (customContext && Object.keys(customContext as BeaconSchema).length !== 0) {
-        // override payload on custom context
-        payload = customContext as BeaconSchema;
+        let payload: BeaconSchema = {};
+        if (customContext && Object.keys(customContext as BeaconSchema).length !== 0) {
+            // override payload on custom context
+            payload = customContext as BeaconSchema;
+        }
+
+        payload.commerce = payload.commerce || {};
+
+        payload.commerce.order = createOrder(payload.commerce.order, orderContext, storefrontInstanceContext);
+        payload.commerce.order.discountAmount =
+            Number(payload.commerce.order.discountAmount || shoppingCartContext?.discountAmount || 0);
+        payload.commerce.promotionID = payload.commerce.promotionID || orderContext?.appliedCouponCode;
+        payload.commerce.shipping = payload.commerce.shipping || {};
+        payload.commerce.shipping.shippingMethod =
+            payload.commerce.shipping.shippingMethod || orderContext?.shipping?.shippingMethod;
+        payload.commerce.shipping.shippingAmount =
+            payload.commerce.shipping.shippingAmount || Number(orderContext?.shipping?.shippingAmount) || 0;
+
+        payload.personalEmail = payload.personalEmail || {};
+        payload.personalEmail.address = payload.personalEmail.address || accountContext?.emailAddress;
+
+        payload.productListItems = createProductListItems(
+            payload.productListItems,
+            shoppingCartContext,
+            undefined,
+            storefrontInstanceContext,
+        );
+
+        payload.commerce.purchases = {
+            value: 1,
+        };
+
+        payload.commerce.commerceScope = createCommerceScope(storefrontInstanceContext);
+
+        payload._id = debugContext?.eventId;
+        payload.eventType = XDM_EVENT_TYPE;
+
+        sendEvent(payload, event);
+    } catch (e) {
+        console.warn("Error in placeOrderAEP handler");
     }
-
-    payload.commerce = payload.commerce || {};
-
-    payload.commerce.order = createOrder(payload.commerce.order, orderContext, storefrontInstanceContext);
-    payload.commerce.order.discountAmount =
-        payload.commerce.order.discountAmount || shoppingCartContext?.discountAmount || 0;
-    payload.commerce.promotionID = payload.commerce.promotionID || orderContext?.appliedCouponCode;
-    payload.commerce.shipping = payload.commerce.shipping || {};
-    payload.commerce.shipping.shippingMethod =
-        payload.commerce.shipping.shippingMethod || orderContext?.shipping?.shippingMethod;
-    payload.commerce.shipping.shippingAmount =
-        payload.commerce.shipping.shippingAmount || Number(orderContext?.shipping?.shippingAmount) || 0;
-
-    payload.personalEmail = payload.personalEmail || {};
-    payload.personalEmail.address = payload.personalEmail.address || accountContext?.emailAddress;
-
-    payload.productListItems = createProductListItems(
-        payload.productListItems,
-        shoppingCartContext,
-        undefined,
-        storefrontInstanceContext,
-    );
-
-    payload.commerce.purchases = {
-        value: 1,
-    };
-
-    payload.commerce.commerceScope = createCommerceScope(storefrontInstanceContext);
-
-    payload._id = debugContext?.eventId;
-    payload.eventType = XDM_EVENT_TYPE;
-
-    sendEvent(payload, event);
 };
 
 export default aepHandler;
