352879dd315d4a2b8fe03049a6ca1dc0f42c8785
Benjamin Kalk <bkalk@adobe.com>
2024-04-23 10:00:43 -0500

    update other relevant handlers to have correct discount amount

--- packages/storefront-events-collector/src/handlers/shoppingCart/viewAEP.ts
+++ packages/storefront-events-collector/src/handlers/shoppingCart/viewAEP.ts
@@ -9,39 +9,43 @@ const XDM_EVENT_TYPE = "commerce.productListViews";
 
 /** shoppingCartView handler for AEP event */
 const aepHandler = async (event: Event): Promise<void> => {
-    const { shoppingCartContext, debugContext, storefrontInstanceContext, customContext } = event.eventInfo;
-
-    let payload: BeaconSchema = {};
-    if (customContext && Object.keys(customContext as BeaconSchema).length !== 0) {
-        // override payload on custom context
-        payload = customContext as BeaconSchema;
+    try {
+        const { shoppingCartContext, debugContext, storefrontInstanceContext, customContext } = event.eventInfo;
+    
+        let payload: BeaconSchema = {};
+        if (customContext && Object.keys(customContext as BeaconSchema).length !== 0) {
+            // override payload on custom context
+            payload = customContext as BeaconSchema;
+        }
+    
+        payload.commerce = payload.commerce || {};
+    
+        payload.commerce.order = payload.commerce.order || ({} as Order);
+        payload.commerce.order.discountAmount = Number(shoppingCartContext?.discountAmount || 0);
+    
+        payload.commerce.cart = payload.commerce.cart || {};
+        payload.commerce.cart.cartID = payload.commerce.cart.cartID || shoppingCartContext?.id;
+    
+        payload.productListItems = createProductListItems(
+            payload.productListItems,
+            shoppingCartContext,
+            undefined,
+            storefrontInstanceContext,
+        );
+    
+        payload.commerce.productListViews = {
+            value: 1,
+        };
+    
+        payload.commerce.commerceScope = createCommerceScope(storefrontInstanceContext);
+    
+        payload._id = debugContext?.eventId;
+        payload.eventType = XDM_EVENT_TYPE;
+    
+        sendEvent(payload, event);
+    } catch (e) {
+        console.warn("Error in shoppingCart view AEP handler");
     }
-
-    payload.commerce = payload.commerce || {};
-
-    payload.commerce.order = payload.commerce.order || ({} as Order);
-    payload.commerce.order.discountAmount = shoppingCartContext?.discountAmount || 0;
-
-    payload.commerce.cart = payload.commerce.cart || {};
-    payload.commerce.cart.cartID = payload.commerce.cart.cartID || shoppingCartContext?.id;
-
-    payload.productListItems = createProductListItems(
-        payload.productListItems,
-        shoppingCartContext,
-        undefined,
-        storefrontInstanceContext,
-    );
-
-    payload.commerce.productListViews = {
-        value: 1,
-    };
-
-    payload.commerce.commerceScope = createCommerceScope(storefrontInstanceContext);
-
-    payload._id = debugContext?.eventId;
-    payload.eventType = XDM_EVENT_TYPE;
-
-    sendEvent(payload, event);
 };
 
 export default aepHandler;
