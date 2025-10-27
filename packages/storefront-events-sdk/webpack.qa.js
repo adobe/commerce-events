a320cfca897152c9e092847818252c77dcf21628
benjaminkalk <bkalk@adobe.com>
2025-10-27 16:52:17 -0500

    Use production mode in QA webpack config (#197)

--- packages/storefront-events-sdk/webpack.qa.js
+++ packages/storefront-events-sdk/webpack.qa.js
@@ -2,6 +2,5 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "eval-source-map",
+    mode: "production",
 });
