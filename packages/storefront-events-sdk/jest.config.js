ff19f84479644ea314e34963a1e8b847bd7b7b80
benjaminkalk <bkalk@adobe.com>
2026-04-29 12:08:36 -0500

    DINT-1889: update ACDL version (#204)
    * DINT-1889: update ACDL version
    
    * Update turbo config and use correct artifactory setup
    
    * Add npmrc file
    
    * update config and lock file for public consumption
    
    * lint fix
    
    * Update tests to mock structuredClone
    
    ---------
    
    Co-authored-by: ravpatel <ravpatel@adobe.com>

--- packages/storefront-events-sdk/jest.config.js
+++ packages/storefront-events-sdk/jest.config.js
@@ -4,6 +4,11 @@ const config = {
     coverageProvider: "v8",
     preset: "ts-jest",
     testEnvironment: "jsdom",
+
+    moduleNameMapper: {
+        "@adobe/adobe-client-data-layer":
+            "<rootDir>/../../node_modules/@adobe/adobe-client-data-layer/dist/adobe-client-data-layer.min.js",
+    },
 };
 
 module.exports = config;
